1956903b62dc795cd626ca2a573eb46c78fc84ae
Brandon Fowler <brandfowl@gmail.com>
2023-09-12 01:33:10 -0400

    Improve input component

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -6,14 +6,21 @@ import { EventEmitter, Component, Input, Output } from '@angular/core';
 	styleUrls: ['./input.component.scss']
 })
 export class InputComponent {
+	static PRECISION = 1000;
+
 	@Input() name?: string;
-	@Input() value?: number;
+	@Input() value: number = 0;
+	@Input() min: number = -Infinity;
 
 	@Output() valueChange = new EventEmitter<number>();
 
 	updateValue(e: Event) {
 		const el = e.target as HTMLInputElement;
 
-		this.valueChange.emit(el.valueAsNumber / 100);
+		this.valueChange.emit(Math.max(this.min, el.valueAsNumber / 100));
+	}
+
+	get displayValue() {
+		return Math.round(this.value * 100 * InputComponent.PRECISION) / InputComponent.PRECISION;
 	}
 }
